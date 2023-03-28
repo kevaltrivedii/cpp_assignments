63cc6b113d59ee452bfd528cada5865202b8a56e
kevaltrivedii <tkeval1610@gmail.com>
2023-03-28 20:40:10 +0530

    program completed

---
+++ M6 program3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Interest {
+private:
+    int principal;
+    int year;
+    float rate;
+
+public:
+    // Constructor with dynamic initialization (rate is with default value of 2.5%)
+    Interest(int p, int y, float r = 2.5) {
+        this->principal = p;
+        this->year = y;
+    }
+    //applied simple interest formula
+    float calculateSimpleInterest() {
+        return (principal * year * 2.5) / 100;
+    }
+};
+
+int main() {
+    int p, y;
+    float r,SI;
+
+    cout << "Enter principal: ";
+    cin >> p;
+    cout << "Enter year: ";
+    cin >> y;
+    
+    //call interest function formula
+    Interest obj(p, y);
+     SI = obj.calculateSimpleInterest();
+
+    cout << "Simple Interest = " << SI;
+
+    return 0;
+}
