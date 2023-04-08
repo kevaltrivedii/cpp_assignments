05b703dba39e53a076cdb9b6e4da2bb17ca90b00
Keval Trivedi <tkeval1610@gmail.com>
2023-04-08 12:29:54 +0530

    Update M7 program2.cpp

--- M7 program2.cpp
+++ M7 program2.cpp
@@ -1,29 +1,32 @@
+// Module 7 Q-2 Write a program of to sort the array using templates
+
+
 #include <iostream>
-#include <algorithm>
+using namespace std;
 
 template<typename T, size_t N>
 void sortArray(T (&arr)[N]) {
-    std::sort(arr, arr+N);
+    sort(arr, arr+N);
 }
 
 int main() {
     int intArr[] = {5, 2, 7, 1, 8};
     sortArray(intArr);
 
-    std::cout << "Sorted int array: ";
+    cout << "Sorted int array: ";
     for (int i = 0; i < 5; i++) {
-        std::cout << intArr[i] << " ";
+        cout << intArr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << std::endl;
 
     float floatArr[] = {3.14, 1.414, 10.0, 2.718, 6.023};
     sortArray(floatArr);
 
-    std::cout << "Sorted float array: ";
+    cout << "Sorted float array: ";
     for (int i = 0; i < 5; i++) {
-        std::cout << floatArr[i] << " ";
+        cout << floatArr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
