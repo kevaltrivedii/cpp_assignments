57165f0264a8af3b1f78e0abf1c4642e47c0584f
Keval Trivedi <tkeval1610@gmail.com>
2023-04-08 12:13:55 +0530

    Update M6 program8.cpp

--- M6 program8.cpp
+++ M6 program8.cpp
@@ -1,3 +1,9 @@
+// Module 6 Q-8 Create a class person having members name and age.
+// Derive a class student having member percentage.
+// Derive another class teacher having member salary. Write necessary member function to initialize, read and write data.
+// Write also Main function (Multiple Inheritance)
+
+
 #include<iostream>
 using namespace std;
 
