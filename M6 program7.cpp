22340b65de7efe0c018a262604d587f3d3f1c9be
Keval Trivedi <tkeval1610@gmail.com>
2023-04-08 12:12:20 +0530

    Update M6 program7.cpp

--- M6 program7.cpp
+++ M6 program7.cpp
@@ -1,3 +1,7 @@
+// Module 6 Q-7  Assume a class cricketer is declared. Declare a derived class batsman from cricketer .
+// Data member of batsman. Total runs, Average runs and best performance. 
+// Member functions input data, calculate average runs, Display data. (Single Inheritance)
+
 
 #include <iostream>
 #include <string>
